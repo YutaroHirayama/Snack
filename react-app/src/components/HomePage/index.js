f8a08144e539f6d94f120a2db55cca43c18042d8
Coverman9 <usubalieve@gmail.com>
2023-05-07 18:12:33 -0500

    channel component changes

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,45 +1,29 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import Channels from "../Channels"
+import Channels from "../Channels";
+import Navigation from "../Navigation";
+
+const HomePage = ({ user, isLoaded }) => {
+  const channelsOwned = user.channelsOwned;
+  const channels = channelsOwned;
 
 
-const HomePage = () => {
-    const user = useSelector(state => state.session.user);
-    const channelsOwned = user.channelsOwned;
-    const channels = channelsOwned;
-
-    const [currentChannel, setCurrentChannel] = useState(null)
-
-    const selectChannel = (id) => {
-        console.log("HERE")
-        console.log(id)
-        // Populate messages to selected channel
-    }
-    useEffect(() => {
-        console.log(currentChannel)
-    }, [currentChannel])
-
-    if (!user) return null
-    return (
-
-        <div id="home-page-container">
-            <h1>HOME PAGE</h1>
-            <div id="left-side-container">
-                <div id="actions-container">
-                </div>
-                <div id="channel-thread-container">
-                    <h2>Channels: </h2>
-                    <Channels channels={channels}
-                        selectChannel={selectChannel}
-                    />
-
-                </div>
-            </div>
-            <div id="messages-container">
-                {/*  <Messages /> */}
-            </div>
+  if (!user) return null;
+  return (
+    <>
+    <Navigation isLoaded={isLoaded}/>
+      <div id="home-page-container">
+        <h1>HOME PAGE</h1>
+        <div id="left-side-container">
+          <div id="actions-container"></div>
+          <div id="channel-thread-container">
+            <h2>Channels: </h2>
+            <Channels channels={channels} />
+          </div>
         </div>
-
-    )
-}
+        <div id="messages-container">{/*  <Messages /> */}</div>
+      </div>
+    </>
+  );
+};
 export default HomePage;
