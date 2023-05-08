8afa93e00b231458371d3c822e0911f51c35a373
Coverman9 <usubalieve@gmail.com>
2023-05-08 17:59:57 -0500

    channel tabs

---
+++ react-app/src/components/ChannelInfoModal/EditChannelModal.js
@@ -0,0 +1,107 @@
+import "./ChannelInfoModal.css";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { editChannelThunk } from "../../store/session";
+import { useModal } from "../../context/Modal";
+
+const EditChannelModal = ({ channel }) => {
+  console.log(channel.members);
+  const [channelName, setChannelName] = useState(channel.channelName);
+  const [description, setDescription] = useState(channel.description);
+  const [tab, setTab] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [channelUsers, setChannelUsers] = useState(channel.members);
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await fetch("/api/users/");
+    const allUsers = await res.json();
+    setUsers(allUsers);
+  };
+
+  const addUser = (user) => {
+    setChannelUsers([...channelUsers, user]);
+  };
+
+  if (!users?.users?.length) return null;
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const updatedChannel = {
+      id: channel.id,
+      channelName,
+      description,
+    };
+    dispatch(editChannelThunk(updatedChannel)).then(closeModal);
+  };
+  return (
+    <>
+      <form onSubmit={formSubmit}>
+        <h3>Update Channel</h3>
+        <label>
+          Channel Name
+          <input
+            type="text"
+            value={channelName}
+            onChange={(e) => setChannelName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Channel Description
+          <textarea
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </label>
+
+        <div id="allusers-channelusers-tab">
+          <div>
+            <h3 onClick={() => setTab(false)}>Channel Users</h3>
+          </div>
+          <div>
+            <h3 onClick={() => setTab(true)}>All Users</h3>
+          </div>
+        </div>
+        <div id="edit-channel-all-users">
+          {tab === true && users["users"].map((user) => (
+            <div>
+              <div>
+                {user.firstName}, {user.lastName}
+              </div>
+              <button
+                disabled={channelUsers.includes(user.id)}
+                onClick={() => addUser(user.id)}
+              >
+                Add
+              </button>
+            </div>
+          ))}
+          {tab === false && channel.members.map((user) => (
+            <div>
+              <div>
+                {user.firstName}, {user.lastName}
+              </div>
+              <button
+                disabled={channelUsers.includes(user.id)}
+                onClick={() => addUser(user.id)}
+              >
+                Add
+              </button>
+            </div>
+          ))}
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default EditChannelModal;
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -2,182 +2,208 @@
 const SET_USER = "session/SET_USER";
 const REMOVE_USER = "session/REMOVE_USER";
 
-
 const CREATE_CHANNEL = "channel/CREATE_CHANNEL";
 const DELETE_CHANNEL = "channel/DELETE_CHANNEL";
+const EDIT_CHANNEL = "channel/EDIT_CHANNEL";
 
 const setUser = (user) => ({
-	type: SET_USER,
-	payload: user,
+  type: SET_USER,
+  payload: user,
 });
 
 const removeUser = () => ({
-	type: REMOVE_USER,
+  type: REMOVE_USER,
 });
 
 // ========= channels
 
 export const createChannelAction = (channel) => ({
-	type: CREATE_CHANNEL,
-	channel
-})
+  type: CREATE_CHANNEL,
+  channel,
+});
 
-const deleteChannelAction = channelId => ({
-	type: DELETE_CHANNEL,
-	channelId
-})
+const deleteChannelAction = (channelId) => ({
+  type: DELETE_CHANNEL,
+  channelId,
+});
 
 export const createChannelThunk = (channel) => async (dispatch) => {
-	const { channelName, isDm, description, addUsers } = channel
-	const res = await fetch("/api/channels", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			channelName,
-			isDm,
-			description,
-			addUsers
-		})
-	})
-	if (res.ok) {
-		const newChannel = await res.json()
-		dispatch(createChannelAction(newChannel))
-	} else {
-		const errors = await res.json();
-		return errors;
-	}
-}
+  const { channelName, isDm, description, addUsers } = channel;
+  const res = await fetch("/api/channels", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      channelName,
+      isDm,
+      description,
+      addUsers,
+    }),
+  });
+  if (res.ok) {
+    const newChannel = await res.json();
+    dispatch(createChannelAction(newChannel));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
 
-export const deleteChannelThunk = channelId => async dispatch => {
-	const res = await fetch(`/api/channels/${channelId}`, {
-		method: "DELETE"
-	})
-	if (res.ok) {
-		dispatch(deleteChannelAction(channelId))
-	} else {
-		const errors = await res.json();
-		return errors;
-	}
-}
+export const editChannelThunk = (channel) => async (dispatch) => {
+  const { channelName, isDm, description, addUsers } = channel;
+  const res = await fetch(`/api/channels/${channel.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      channelName,
+      description,
+    }),
+  });
+  if (res.ok) {
+    const updatedChannel = await res.json();
+    dispatch(createChannelAction(updatedChannel));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
+
+export const deleteChannelThunk = (channelId) => async (dispatch) => {
+  const res = await fetch(`/api/channels/${channelId}`, {
+    method: "DELETE",
+  });
+  if (res.ok) {
+    dispatch(deleteChannelAction(channelId));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
 
 // =============== AUTH
 
-
 export const authenticate = () => async (dispatch) => {
-	const response = await fetch("/api/auth/", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-	if (response.ok) {
-		const data = await response.json();
-		if (data.errors) {
-			return;
-		}
+  const response = await fetch("/api/auth/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    if (data.errors) {
+      return;
+    }
 
-		dispatch(setUser(data));
-	}
+    dispatch(setUser(data));
+  }
 };
 
 export const login = (email, password) => async (dispatch) => {
-	const response = await fetch("/api/auth/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			email,
-			password,
-		}),
-	});
+  const response = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
 
-	if (response.ok) {
-		const data = await response.json();
-		dispatch(setUser(data));
-		return null;
-	} else if (response.status < 500) {
-		const data = await response.json();
-		if (data.errors) {
-			return data.errors;
-		}
-	} else {
-		return ["An error occurred. Please try again."];
-	}
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setUser(data));
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ["An error occurred. Please try again."];
+  }
 };
 
 export const logout = () => async (dispatch) => {
-	const response = await fetch("/api/auth/logout", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+  const response = await fetch("/api/auth/logout", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-	if (response.ok) {
-		dispatch(removeUser());
-	}
+  if (response.ok) {
+    dispatch(removeUser());
+  }
 };
 
-export const signUp = (username, email, password, firstName, lastName, profilePic) => async (dispatch) => {
-	const response = await fetch("/api/auth/signup", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			username,
-			email,
-			password,
-			firstName,
-			lastName,
-			profilePic
-		}),
-	});
+export const signUp =
+  (username, email, password, firstName, lastName, profilePic) =>
+  async (dispatch) => {
+    const response = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        firstName,
+        lastName,
+        profilePic,
+      }),
+    });
 
-	if (response.ok) {
-		const data = await response.json();
-		dispatch(setUser(data));
-		return null;
-	} else if (response.status < 500) {
-		const data = await response.json();
-		if (data.errors) {
-			return data.errors;
-		}
-	} else {
-		return ["An error occurred. Please try again."];
-	}
-};
+    if (response.ok) {
+      const data = await response.json();
+      dispatch(setUser(data));
+      return null;
+    } else if (response.status < 500) {
+      const data = await response.json();
+      if (data.errors) {
+        return data.errors;
+      }
+    } else {
+      return ["An error occurred. Please try again."];
+    }
+  };
 
 const initialState = { user: null };
 
 export default function reducer(state = initialState, action) {
-	switch (action.type) {
-		case SET_USER: {
-			const newState = { user: action.payload };
-			console.log("NEWSTATE: ", newState)
-			const newChannels = {}
-			newState.user.channels.forEach(channel => {
-				newChannels[channel.id] = channel
-			});
-			newState.user.channels = newChannels
-			return newState;
-		}
-		case REMOVE_USER:
-			return { user: null };
-		case CREATE_CHANNEL:
-			{
-				const newState = { ...state, user: { ...state.user } };
-				newState.user.channels = { ...state.user.channels, [action.channel.channel.id]: action.channel.channel };
-				return newState;
-			}
-		case DELETE_CHANNEL:
-			{
-				const newState = { ...state, user: { ...state.user, channels: { ...state.user.channels } } };
-				delete newState.user.channels[action.channelId];
-				return newState;
-			}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SET_USER: {
+      const newState = { user: action.payload };
+      console.log("NEWSTATE: ", newState);
+      const newChannels = {};
+      newState.user.channels.forEach((channel) => {
+        newChannels[channel.id] = channel;
+      });
+      newState.user.channels = newChannels;
+      return newState;
+    }
+    case REMOVE_USER:
+      return { user: null };
+    case CREATE_CHANNEL: {
+      const newState = { ...state, user: { ...state.user } };
+      newState.user.channels = {
+        ...state.user.channels,
+        [action.channel.channel.id]: action.channel.channel,
+      };
+      return newState;
+    }
+    case DELETE_CHANNEL: {
+      const newState = {
+        ...state,
+        user: { ...state.user, channels: { ...state.user.channels } },
+      };
+      delete newState.user.channels[action.channelId];
+      return newState;
+    }
+    default:
+      return state;
+  }
 }
