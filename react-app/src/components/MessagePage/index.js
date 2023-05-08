13419b50c2c106c518da3c471802e92306d0db19
Yutaro <yutarohirayama@gmail.com>
2023-05-08 09:02:11 -0700

    messages-frontend progress

---
+++ react-app/src/components/MessagePage/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./MessagePage.css";
+
+
+const MessagePage = () => {
+
+    const messages = useSelector(state => state.Messages)
+
+
+
+
+
+    return (
+        <>
+        <h2>channel name</h2>
+        </>
+
+    )
+}
