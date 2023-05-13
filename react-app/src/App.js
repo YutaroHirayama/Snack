1f2171ea8547fd89cb5b3dc8e0f322fae37ecf2f
Coverman9 <usubalieve@gmail.com>
2023-05-12 19:28:57 -0500

    main page css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
-import MessagePage from "./components/MessagePage"
+import MessagePage from "./components/MessagePage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import LandingPage from "./components/LandingPage";
@@ -12,13 +12,11 @@ import { Redirect } from "react-router-dom";
 import ThreadsPage from "./components/ThreadsPage";
 import Channels from "./components/Channels";
 import { io } from "socket.io-client";
+import "./index.css";
 
-let socket
-
+let socket;
 
 function App() {
-
-
   const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -31,31 +29,42 @@ function App() {
     <>
       {isLoaded && (
         <>
-          <Route path="/">
-            {sessionUser &&
-              <>
-                <Navigation isLoaded={isLoaded} />
-                <Channels channels={sessionUser.channels} user={sessionUser} isLoaded={isLoaded} socket={socket} />
-              </>
-            }
-          </Route>
-          <Switch>
-            <Route exact path="/">
-              {!sessionUser && <LandingPage isLoaded={isLoaded} />}
+          <div className="home-page-main">
+            <div>
+            <Route path="/">
+              {sessionUser && (
+                <>
+                  <Navigation isLoaded={isLoaded} />
+                  <Channels
+                    channels={sessionUser.channels}
+                    user={sessionUser}
+                    isLoaded={isLoaded}
+                    socket={socket}
+                  />
+                </>
+              )}
             </Route>
-          </Switch>
-          <Route path="/channel/:channelId">
-            {!sessionUser && <Redirect to="/" />}
-            <MessagePage user={sessionUser} />
-          </Route>
-          <Route path="/channel/:channelId/message/:messageId">
-            {!sessionUser && <Redirect to="/" />}
-            <ThreadsPage user={sessionUser} />
-          </Route>
-          <Route exact path="/test">
-            <h1>TEST</h1>
-          </Route>
+            </div>
 
+            <Switch>
+              <Route exact path="/">
+                {!sessionUser && <LandingPage isLoaded={isLoaded} />}
+              </Route>
+            </Switch>
+
+            <div>
+            <Route path="/channel/:channelId">
+              {!sessionUser && <Redirect to="/" />}
+              <MessagePage user={sessionUser} />
+            </Route>
+            </div>
+            <div>
+            <Route path="/channel/:channelId/message/:messageId">
+              {!sessionUser && <Redirect to="/" />}
+              <ThreadsPage user={sessionUser} />
+            </Route>
+            </div>
+          </div>
         </>
       )}
     </>
