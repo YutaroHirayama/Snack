5827a40312dc3e1095e7a1db43fb7c4212b39571
Coverman9 <usubalieve@gmail.com>
2023-05-13 16:36:34 -0500

    edit message css

--- react-app/src/components/Message/EditMessageModal.js
+++ react-app/src/components/Message/EditMessageModal.js
@@ -6,43 +6,52 @@ import "./Message.css";
 import { useHistory } from "react-router-dom";
 
 const EditMessageModal = ({ message, socket }) => {
-    const history = useHistory();
-    const { closeModal } = useModal();
+  const history = useHistory();
+  const { closeModal } = useModal();
 
-    const [_message, setMessage] = useState(message.message);
+  const [_message, setMessage] = useState(message.message);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const messageSubmit = async e => {
-        e.preventDefault();
-        const newMessage = await dispatch(editMessageThunk(message, _message));
-        if (newMessage.no_message) {
-            alert("Channel no longer exists");
-            closeModal();
-            history.push(`/`);
-            return
-        }
-        socket.emit('chat', newMessage);
-        // setMessage('');
-        closeModal()
+  const messageSubmit = async (e) => {
+    e.preventDefault();
+    const newMessage = await dispatch(editMessageThunk(message, _message));
+    if (newMessage.no_message) {
+      alert("Channel no longer exists");
+      closeModal();
+      history.push(`/`);
+      return;
     }
+    socket.emit("chat", newMessage);
+    // setMessage('');
+    closeModal();
+  };
 
-    const messageTooLong = _message.length > 10000
+  const messageTooLong = _message.length > 10000;
 
-
-    return (
+  return (
+    <>
+      <div className="edit-message-form-modal">
         <form onSubmit={messageSubmit}>
-            <textarea
-                className="message-textarea"
-                value={_message}
-                onChange={e => setMessage(e.target.value)}
-                placeholder="Enter message"
-            ></textarea>
-            {messageTooLong && <p>{`Message is ${_message.length - 10000} characters too long.`}</p>}
-            <button
-                type='submit'
-                disabled={!_message || messageTooLong}>Send</button>
+          <textarea
+            className="message-textarea"
+            value={_message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Enter message"
+          ></textarea>
+          {messageTooLong && (
+            <p>{`Message is ${
+              _message.length - 10000
+            } characters too long.`}</p>
+          )}
+          <div className="edit-messsage-send-button">
+            <button type="submit" disabled={!_message || messageTooLong}>
+              Send
+            </button>
+          </div>
         </form>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default EditMessageModal;
