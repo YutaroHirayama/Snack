8eb6cfd840a217c57c431e2069fcb192134fbf53
Coverman9 <usubalieve@gmail.com>
2023-05-13 03:23:47 -0500

    reactions css

--- react-app/src/components/Reaction/ReactionFormModal.js
+++ react-app/src/components/Reaction/ReactionFormModal.js
@@ -1,10 +1,11 @@
 import Reaction from './index';
+import "./ReactionFormModal.css"
 
 const ReactionFormModal = ({message, socket}) => {
     let emojis = ["👍🏻", "👎🏻", "😂", "💩", "🇺🇸", "🇯🇵", "🇰🇬", "🇲🇩", "😍", "🤌🏻", "🙏🏻", "❤️", "😎"];
 
     return (
-        <div>
+        <div className='reaction-form-modal'>
             <h2>Choose Reaction</h2>
             {emojis.map(emoji =>
                 <Reaction reaction={emoji} message={message} socket={socket}/>
--- react-app/src/components/Reaction/index.js
+++ react-app/src/components/Reaction/index.js
@@ -2,39 +2,43 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addReactionThunk, removeReactionThunk } from "../../store/channels";
 import { useModal } from "../../context/Modal";
+import "./ReactionFormModal.css"
 
-const Reaction = ({reaction, count, message, socket}) => {
+const Reaction = ({ reaction, count, message, socket }) => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const [used, setUsed] = useState(false);
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
-  const sessionUser = useSelector((state) => state.session.user)
-  const [used, setUsed] = useState(false)
-  const dispatch = useDispatch()
-  const { closeModal } = useModal()
-
-  const messageId = message.id
-  const userId = sessionUser.id
-  let existingReaction = message?.reactions?.find(r => r.userId === userId && r.reaction === reaction)
-  let reactionId = existingReaction?.id
+  const messageId = message.id;
+  const userId = sessionUser.id;
+  let existingReaction = message?.reactions?.find(
+    (r) => r.userId === userId && r.reaction === reaction
+  );
+  let reactionId = existingReaction?.id;
 
   const onClick = async (e) => {
-    e.preventDefault()
-      if(!reactionId) {
-        const newReaction = await dispatch(addReactionThunk(reaction, messageId, userId))
-        socket.emit('chat', newReaction)
-        closeModal()
-      } else {
-      if(reactionId){
-        await dispatch(removeReactionThunk(reactionId, messageId))
-        socket.emit('chat', 'DELETED')
-        closeModal()
+    e.preventDefault();
+    if (!reactionId) {
+      const newReaction = await dispatch(
+        addReactionThunk(reaction, messageId, userId)
+      );
+      socket.emit("chat", newReaction);
+      closeModal();
+    } else {
+      if (reactionId) {
+        await dispatch(removeReactionThunk(reactionId, messageId));
+        socket.emit("chat", "DELETED");
+        closeModal();
       }
-     }
-  }
-    return (
-        <>
-          <button
-            onClick={onClick}
-          >{reaction} {count}</button>
-        </>
-    )
-}
-export default Reaction
+    }
+  };
+  return (
+    <>
+      <button className="actuall-reaction" onClick={onClick}>
+        {reaction} {count}
+      </button>
+    </>
+  );
+};
+export default Reaction;
