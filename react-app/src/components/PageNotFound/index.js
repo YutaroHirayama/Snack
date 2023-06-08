975be417196e4629574032b79e53336189b3fa64
Yutaro <yutarohirayama@gmail.com>
2023-06-08 16:43:53 -0700

    css for 404

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -5,9 +5,7 @@ const PageNotFound = () => {
 
   return (
     <div className='page-not-found'>
-      <div className='page-not-found-image'>
         <img src='/PageNotFound.jpg' className='error-page-image'/>
-      </div>
     </div>
   );
 };
